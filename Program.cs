77ff0d826bcf72914d95a959e9bd00e0f4814006
yücel <ytoluyag@gmail.com>
2022-04-04 01:52:30 +0300

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+
+namespace OOPGiris
+{
+     class Program
+    {
+        static void Main()
+        {
+            Program p = new Program();
+            int sonuc = p.topla(3, 2);
+            Console.WriteLine(sonuc);
+            Console.ReadKey();
+        }
+
+        private int topla (int a, int b)
+        {
+            return a + b;
+        }
+    }
+}
