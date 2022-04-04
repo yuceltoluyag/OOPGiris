a77c2a3fe8b9424625ecda56a5d019d83558ea61
yücel <ytoluyag@gmail.com>
2022-04-04 21:25:39 +0300

    Inheritance Metot

---
+++ InheritanceMetod.cs
@@ -0,0 +1,54 @@
+﻿using System;
+
+
+namespace ClassTuttorial
+{
+    // Inheritance Metot 
+
+    public class BaseClass
+    {
+        // Cons
+        /*
+         *  Türetilmiş sınıftan bir yapıcı method oluşturulup çağrılırsa
+         *  Öncelikle BaseClassın içerisinde ki yapıcı method çağrılır.
+         */
+
+        public BaseClass()
+        {
+            Console.WriteLine("BaseClass Yapıcı Method Çalıştırıldı");
+        }
+
+        // Paremetri şekilde deniyoruz acaba hangisi çağrılacak
+
+        /*
+         * Yine Parametresiz olanı çağırdı.
+         * Eğer parametreli olanı çağırmak istiyorsa base anahtar sözcüğünü kullanmalıyız.
+         */
+
+        public BaseClass(string Message)
+        {
+            Console.WriteLine(Message);
+        }
+
+    }
+
+    public class DerivedClass : BaseClass
+    {
+        public DerivedClass() : base("Parametreli olan") //Parametreli olanı çağırıyoruz
+        {
+            Console.WriteLine("DerivedClass Yapıcı Method Çalıştırıldı");
+
+        }
+    }
+    class Program
+    {
+        static void Main()
+        {
+            DerivedClass dcs = new DerivedClass();
+
+            Console.ReadKey();
+        }
+    }
+
+}
+
