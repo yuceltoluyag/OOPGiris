9811772e5f036defc61b1c9e3be48452a38a2f7f
yücel <ytoluyag@gmail.com>
2022-04-04 05:00:16 +0300

    staticmembers ve daire alanı hesaplama

---
+++ Daire.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace ClassTuttorial
+{
+    public class Daire
+    {
+
+        /*
+         * Static değerlere sınıf ismi üzerinden ulaşılır.
+         * Static Bir method içerisinden static olmayan bir veri yada değişken çağrılamaz.
+         * Static yapıcı methodların erişim belirteci olmaz ve parametrede almazlar. Public,private vs
+         */
+        int _r;
+        static float _PI = 3.14f;
+
+        static Daire()
+        {
+            Daire._PI = 3.14f;
+            Console.WriteLine("Yapıcı Method Çağrıldı");
+        }
+
+        // constr
+        public Daire(int r)
+        {
+            this._r = r;
+        }
+
+        public float AlanHesapla()
+        {
+            return this._r * this._r * Daire._PI;
+        }
+    }
+}
---
+++ StaticMembers.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+
+namespace ClassTuttorial
+{
+
+
+    class Program
+    {
+        static void Main()
+        {
+            Daire d1 = new Daire(2);
+            float alan1 = d1.AlanHesapla();
+            Console.WriteLine(alan1);
+
+            Daire d2 = new Daire(4);
+            float alan2 = d2.AlanHesapla();
+            Console.WriteLine(alan2);
+
+            Console.ReadKey();
+        }
+    }
+}
